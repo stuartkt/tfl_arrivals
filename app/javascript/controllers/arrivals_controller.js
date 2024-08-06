ab5a7b20d6fdcdc416c27bef487f9aed48c55b3c
Stuart Tee <sktee93@gmail.com>
2024-08-06 17:19:50 +0100

    arrival views and stimulus controller

---
+++ app/javascript/controllers/arrivals_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = [ "table" ]
+
+    connect() {
+        this.startRefreshing()
+    }
+
+
+    startRefreshing() {
+        setInterval(() => {
+            this.refreshArrivals()
+        }, 30000) // Update every 30 seconds
+    }
+
+    refreshArrivals() {
+        fetch('/', {
+            headers: {
+                Accept: "text/vnd.turbo-stream.html"
+            }
+        }).then()
+    }
+}
